9f09a577461c41ea09bf398382cc8a3dd053c570
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2023-08-27 15:25:24 -0300

    Fixed tests

--- packages/helios-router-solid/tests/fixtures/routes/NavComponent.tsx
+++ packages/helios-router-solid/tests/fixtures/routes/NavComponent.tsx
@@ -1,11 +1,36 @@
+import { router } from '../router';
+
 export function NavComponent() {
+  function onClick(e: Event) {
+    e.preventDefault();
+
+    router.appStateStore.produce((appState) => {
+      appState.appLinkClicks++;
+    });
+    
+    router.push((e.target as HTMLAnchorElement).getAttribute('href')!);
+  }
+
   return (
     <nav class='NavComponent__box'>
-      <a href='/'>Home</a>
-      <a href='/about'>About</a>
-      <a href='/client/1'>Client 1</a>
-      <a href='/client/2'>Client 2</a>
-      <a href='/client/3?search=randomstuff'>Client 3</a>
+      <a href='/' onclick={onClick}>
+        Home
+      </a>
+      <a href='/about' onclick={onClick}>
+        About
+      </a>
+      <a href='/create-on-effect-test' onclick={onClick}>
+        CreateOnEffectTest
+      </a>
+      <a href='/client/1' onclick={onClick}>
+        Client 1
+      </a>
+      <a href='/client/2' onclick={onClick}>
+        Client 2
+      </a>
+      <a href='/client/3?search=randomstuff' onclick={onClick}>
+        Client 3
+      </a>
     </nav>
   );
 }
