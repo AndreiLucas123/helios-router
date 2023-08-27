308caf353bafff82f733499594b75db2d15679f4
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2023-08-27 16:25:27 -0300

    Tests and publish to rollup-routes-plugin

--- packages/rollup-plugin-helios-router/tests/rollup-routes-plugin.test.ts
+++ packages/rollup-plugin-helios-router/tests/rollup-routes-plugin.test.ts
@@ -1,19 +1,27 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { routesWatcher } from '../dist/index';
+import fs from 'fs';
+import exp from 'constants';
 
 //
 //
 
-test.only('Deve criar o arquivo routes.ts com sucesso sucesso', async ({
-  page,
-}) => {
+test('Deve criar o arquivo routes.ts com sucesso sucesso', async ({ page }) => {
   //
   const watcher = routesWatcher({
-    routesFolder: '../fixtures/app',
+    routesFolder: './fixtures/app',
   });
   watcher.start();
 
   // Wait 100ms to generate routes.ts
   await page.waitForTimeout(100);
   watcher.stop();
+
+  // Check if routes.ts exists
+  const routes = fs.existsSync('./fixtures/app/routes.ts');
+  expect(routes).toBeTruthy();
+
+  // Snapshot of routes content
+  const routesContent = fs.readFileSync('./fixtures/app/routes.ts', 'utf-8');
+  expect(routesContent).toMatchSnapshot();
 });
